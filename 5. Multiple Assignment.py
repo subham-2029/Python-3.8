25d720c27c3d1e6fb0cda2530493a873faade884
subham-2029 <rsatool125@gmail.com>
2025-08-30 09:44:05 +0530

    New !

---
+++ 5. Multiple Assignment.py
@@ -0,0 +1,9 @@
+#Multiple Assignment 
+a, b, c = 5, 10, 15
+print(a)  # Output: 5
+print(b)  # Output: 10
+print(c)  # Output: 15
+print(a + b + c)  # Output: 30
+
+
+
---
+++ 6. Summary of assignment.py
@@ -0,0 +1,14 @@
+num=7
+amt=123.45
+code="A"
+pi=3.1415
+pop_of_india=145000000
+msg="Hi"
+print("num =", num)
+print("amt =", amt)
+print("code =", code)   
+print("pi =", pi)
+print("pop_of_india =", pop_of_india)
+print("msg =", msg)
+print("num =", num, ", amt =", amt, ", code =", code, ", pi =",
+        pi, ", pop_of_india =", pop_of_india, ", msg =", msg)   
